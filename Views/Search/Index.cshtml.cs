ba2b2e1166dc8b2fd3e6ac17e8056850f7f00687
stilyan-23180041 <sstoyanov_23180041@unwe.bg>
2026-03-11 17:00:52 +0200

    pushed

---
+++ Views/Search/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BizSecureDemo22180035.Views.Search
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
