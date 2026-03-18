4673ab3b08e656b10528d86e2ad88ccd37dd5f13
stilyan-23180041 <sstoyanov_23180041@unwe.bg>
2026-03-18 17:18:19 +0200

    aa

---
+++ ViewModels/ReplayDemoVm .cs
@@ -0,0 +1,15 @@
+﻿using BizSecureDemo22180035.ViewModels;
+namespace BizSecureDemo22180035.ViewModels
+{
+    public class ReplayDemoVm
+    {
+        public decimal Balance { get; set; }
+        public string? Message { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; } = 100;
+        public string Token { get; set; } = "SECRET123";
+        public string Nonce { get; set; } = string.Empty;
+       
+
+    }
+}
